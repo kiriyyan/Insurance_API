853a4f4b2648630982952e181df9518380b2b691
kiriyyan <kiriyan54@gmail.com>
2026-02-15 16:33:55 +0300

    refactor: add connection decorator, clean up db_post_client and Client model

--- database.py
+++ database.py
@@ -1,64 +1,56 @@
 import psycopg2
 from config import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 
-def db_get_client(client_id:int):
-    conn = psycopg2.connect(
-        host = DB_HOST,
-        user = DB_USER,
-        password = DB_PASSWORD,
-        port = DB_PORT,
-        dbname = DB_NAME
-    )
-    if conn:
-        print('DATABASE CONNECTED')
+def db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT,
+            dbname=DB_NAME
+        )
+        if conn:
+            print('DATABASE CONNECTED')
+        cursor = conn.cursor()
 
-    cursor = conn.cursor()
+        try:
+            result = func(cursor, conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            raise e
+        finally:
+            cursor.close()
+            conn.close()
+
+    return wrapper
+
+@db_connection
+def db_get_client(cursor, conn, client_id:int):
     sql = f"SELECT * FROM Clients where client_id = %s::INT"
     cursor.execute(sql, (client_id,))
-    result = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    result = cursor.fetchone()
+
     return result
 
 
-
-def db_get_all_clients():
-    conn = psycopg2.connect(
-        host = "127.0.0.1",
-        user = 'postgres',
-        password = 'postgres',
-        port = 5432,
-        dbname = 'insurance'
-    )
-    if conn:
-        print('DATABASE CONNECTED')
-
-    cursor = conn.cursor()
+@db_connection
+def db_get_all_clients(cursor, conn):
     sql = f"SELECT * FROM Clients"
     cursor.execute(sql)
     result = cursor.fetchall()
-    cursor.close()
-    conn.close()
     return result
 
-
-def db_post_client(first_name, last_name, address, phone_number, email):
-    conn = psycopg2.connect(
-        host = "127.0.0.1",
-        user = 'postgres',
-        password = 'postgres',
-        port = 5432,
-        dbname = 'insurance'
-    )
-    if conn:
-        print('DATABASE CONNECTED')
-
-    cursor = conn.cursor()
-    sql = "INSERT INTO clients (first_name, last_name, address, phone_number, email) VALUES(%s, %s, %s, %s, %s)"
+@db_connection
+def db_post_client(cursor, conn, first_name, last_name, address, phone_number, email):
+    sql = "INSERT INTO clients (first_name, last_name, address, phone_number, email) VALUES(%s, %s, %s, %s, %s) RETURNING client_id"
     cursor.execute(sql, (first_name,last_name, address, phone_number, email))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    return cursor.fetchone()[0]
+
+
 
 
 
--- main.py
+++ main.py
@@ -2,38 +2,54 @@
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from models import Client
 
 import database
 
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/clients',tags = ['Clients'], summary="Даёт список клиентов")
 def get_clients():
     try:
-        return database.db_get_all_clients()
+        clients = database.db_get_all_clients()
+        return clients
     except Exception as e:
-        return {'status': e}
+        raise HTTPException(status_code=500, detail='database_error')
 
 @app.get('/clients/{client_id}',tags = ['Clients'], summary="Даёт клиента по Client_id")
 def get_client(client_id:int):
     try:
-        return database.db_get_client(client_id)
+        client = database.db_get_client(client_id)
     except Exception as e:
-        return {'status': e}
-    # raise HTTPException(status_code=404,detail="Клиент не найден")
+        raise HTTPException(status_code=500, detail='database_error')
+    if client:
+        return client
+    else:
+        raise HTTPException(status_code=404, detail='Client not found')
 
 
-@app.post('/clients')
+
+@app.post('/clients', tags = ['Clients'], summary="Создаёт нового клиента", status_code=201)
 def create_client(new_client: Client):
     try:
-        database.db_post_client(new_client.first_name, new_client.last_name, new_client.address, new_client.phone_number, new_client.email)
-        return {'status':'ok'}
+        current_client = database.db_post_client(new_client.first_name, new_client.last_name, new_client.address, new_client.phone_number, new_client.email)
+        return {'status':'ok', 'id': current_client}
     except Exception as e:
-        return {'status':e}
+        raise HTTPException(500, detail='database_error')
+
 
 if __name__ == '__main__':
     uvicorn.run(app, reload = True)
